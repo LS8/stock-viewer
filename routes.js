9a143c38553dbf08d935c372a1411a90c0e8bc13
LS8 <lennartschoettker@hotmail.com>
2017-08-17 08:46:42 +0200

    add route to delete a stock

--- routes.js
+++ routes.js
@@ -3,7 +3,9 @@ const Router = express.Router();
 
 // Controllers
 const createStock = require('./controllers/createStock');
+const deleteStock = require('./controllers/deleteStock');
 
 Router.post('/', createStock);
+Router.delete('/', deleteStock);
 
 module.exports = Router;
