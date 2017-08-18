48812c6490d56680a5bee587c82115d466b58750
LS8 <lennartschoettker@hotmail.com>
2017-08-18 14:08:10 +0200

    add Container that handles api calls and db interaction and no UI

--- react-src/src/Components/Content.js
+++ react-src/src/Components/Content.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import StockWrapperContainer from './StockWrapperContainer';
 import Footer from './Footer';
 
 const Content = () => {
   return (
-    <div className="col-sm-12">
-      <p>CONTENT</p>
+    <div className="content col-sm-12">
+      <StockWrapperContainer />
       <Footer />
     </div>
   );
