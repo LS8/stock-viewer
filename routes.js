ae7b7d1ba325d57488248901334d0efd1854cfa3
LS8 <lennartschoettker@hotmail.com>
2017-08-18 14:05:37 +0200

    add dynamic get route that uses the corresponding controller to fetch finance data from quandl

--- routes.js
+++ routes.js
@@ -6,8 +6,11 @@ const findStock = require('./controllers/findStock');
 const createStock = require('./controllers/createStock');
 const deleteStock = require('./controllers/deleteStock');
 
+const fetchFromQuandl = require('./controllers/quandlStock');
+
 Router.get('/', findStock);
 Router.post('/', createStock);
 Router.delete('/', deleteStock);
+Router.get('/quandl/:symbol', fetchFromQuandl);
 
 module.exports = Router;
