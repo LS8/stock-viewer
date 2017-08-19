30a4dc2530e4897331fc24454597bead3bd5ac7c
LS8 <lennartschoettker@hotmail.com>
2017-08-19 11:42:08 +0200

    add api key

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
   mongoAddress: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/stockMarket',
   port: process.env.PORT || 8080,
+  apiKey: process.env.QUANDL_KEY || require('./secrets.js').apiKey
 };
 
 module.exports = {
