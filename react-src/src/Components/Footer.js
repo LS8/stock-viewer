d78ab8d2dd170a51daebcb8376d34cf2644183c4
LS8 <lennartschoettker@hotmail.com>
2017-08-20 15:19:46 +0200

    style footer link the same as page heading in terms of color and hover

--- react-src/src/Components/Footer.js
+++ react-src/src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="footer col-sm-12">
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/LS8">
+      <a className="footer-link" target="_blank" rel="noopener noreferrer" href="https://github.com/LS8">
         @LS8
       </a>
     </div>
